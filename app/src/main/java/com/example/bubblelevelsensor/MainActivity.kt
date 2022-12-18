18d4ffa8db2c01c5454aa9b9ea22a0f07495d4b5
SALITH CHETTOORU SUBHASH <saljithchettoorusubhash@student.griffith.ie>
2022-12-18 17:42:43 +0530

    current angle

--- app/src/main/java/com/example/bubblelevelsensor/MainActivity.kt
+++ app/src/main/java/com/example/bubblelevelsensor/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bubblelevelsensor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bubblelevelsensor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
